a12201bb448e9632f33b245bd7f564e86c10983c
Bruno Proenca <bruno.proenca@synchro.com.br>
2021-05-08 20:01:14 -0300

    web conf adapter

--- security-gateway/secured-service/src/main/java/com/scg/service/SecurityConfig.java
+++ security-gateway/secured-service/src/main/java/com/scg/service/SecurityConfig.java
@@ -1,25 +1,24 @@
 package com.scg.service;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Bean
-  SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
     http
-        .authorizeExchange()
-          .pathMatchers("/resource")
-            //.hasAuthority("SCOPE_resource.read")
-            .authenticated()
-          .pathMatchers("/open")
+      .authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/actuator/**", "/open/**")
             .permitAll()
-          .anyExchange()
+        .anyRequest()
             .authenticated()
-          .and()
-        .oauth2ResourceServer()
-          .jwt();
-    return http.build();
+        .and()
+      .oauth2ResourceServer()
+      .jwt();
   }
+  
 }
