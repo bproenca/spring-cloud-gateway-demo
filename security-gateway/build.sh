aaa7648fa59dc12896757b2f38c79879e7c4feae
Bruno Proenca <bruno.proenca@synchro.com.br>
2021-05-11 09:22:51 -0300

    upgrade java spring

--- security-gateway/build.sh
+++ security-gateway/build.sh
@@ -5,10 +5,12 @@ echo "Performing a clean Maven build"
 
 echo "Building the Gateway"
 cd security-gateway
+docker rmi scg-bcp-security-gateway:latest
 docker build --tag scg-bcp-security-gateway .
 cd ..
 
 echo "Building the Service"
 cd secured-service
+docker rmi scg-bcp-secured-service:latest
 docker build --tag scg-bcp-secured-service .
 cd ..
